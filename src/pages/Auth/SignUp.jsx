bade4652f3de893b4cd9f5a405dabfde3eef42b9
Ayushpatel2006 <patelayush0310@gmail.com>
2026-03-15 15:50:17 +0530

    Updated login, signup and helper functions

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
 import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
+import { validateEmail } from "../../utils/helper";
 
 const SignUp = ({ setCurrentPage }) => {
     const [profilePic, setProfilePic] = useState(null);
@@ -16,6 +17,32 @@ const SignUp = ({ setCurrentPage }) => {
     // Handle SignUp Form Submit
     const handleSignUp = async (e) => {
         e.preventDefault();
+
+            let profileImageUrl = "";
+
+        if (!fullName) {
+            setError("Please enter your full name.");
+            return;
+        }
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address.");
+            return;
+        };
+        if (!password) {
+            setError("Please enter your password.");
+            return;
+        }
+        setError("");
+
+        //SignUp API Call
+        try {
+        }   catch (error) {
+            if (error.response && error.response.data.message) {
+                setError(error.response.data.message);
+            } else {
+                setError("Something went wrong. Please try again.");
+            }
+        }
     };
 
     return <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
